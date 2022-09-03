23db479326d5119c2c7668de6e75091e4391075e
mehedihasan369 <mehedihasan6472018@gmail.com>
2022-09-03 08:29:35 +0600

    card-created

--- news.js
+++ news.js
@@ -1,5 +1,6 @@
 const loadCategories = () =>{
     
+    // const url = `https://openapi.programming-hero.com/api/news/categories`
     const url = `https://openapi.programming-hero.com/api/news/categories`
     fetch(url)
     .then(res => res.json())
@@ -11,11 +12,28 @@ const displayCategoies = catagory =>{
     const catagoriesContainer = document.getElementById('catagory')
     catagory.forEach(news_category => {
         
-         const catagoryBtn = document.createElement('button');
+         const catagoryBtn = document.createElement('div');
          catagoryBtn.classList.add('categoryBtn');
-         catagoryBtn.innerHTML = `${news_category.category_name}`;
+         catagoryBtn.innerHTML = `<button onclick="onclickCategory()" type="button"  class="btn btn-light text-secondary">${news_category.category_name}</button>`;
          catagoriesContainer.appendChild(catagoryBtn);
+        
     });
+
+    
+}
+
+///>>>>>>>>>>>>>News cards------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// const loadCategories = () =>{
+//     const url = `https://openapi.programming-hero.com/api/news/categories`
+//     fetch(url)
+//     .then(res => res.json())
+//     .then(status => displayCategoies(status.data.news_category));
+// }
+
+// ////-----------------------
+const onclickCategory =() =>{
+    // console.log('abc');বাকি আছে 
+
 }
 
 
