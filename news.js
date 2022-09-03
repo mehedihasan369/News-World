776fd69a873312139f3b97949c88b57832de15aa
mehedihasan369 <mehedihasan6472018@gmail.com>
2022-09-03 09:58:18 +0600

    cardlinked

--- news.js
+++ news.js
@@ -1,6 +1,4 @@
 const loadCategories = () =>{
-    
-    // const url = `https://openapi.programming-hero.com/api/news/categories`
     const url = `https://openapi.programming-hero.com/api/news/categories`
     fetch(url)
     .then(res => res.json())
@@ -14,27 +12,35 @@ const displayCategoies = catagory =>{
         
          const catagoryBtn = document.createElement('div');
          catagoryBtn.classList.add('categoryBtn');
-         catagoryBtn.innerHTML = `<button onclick="onclickCategory()" type="button"  class="btn btn-light text-secondary">${news_category.category_name}</button>`;
+         catagoryBtn.innerHTML = `<button onclick="onclickCategory(${news_category.category_id})" type="button"  class="btn btn-light text-secondary">${news_category.category_name}</button>`;
          catagoriesContainer.appendChild(catagoryBtn);
+        ////----------
         
+        ///--------
     });
+}
 
+///catagory id calling--------
+const onclickCategory =(category_id) =>{ 
+    // console.log(category_id)
+    loadArticles(category_id)
+}
+///>>>>>>>>>>>>>News cards------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+const loadArticles = (categoryId) =>{
+    const url = `https://openapi.programming-hero.com/api/news/category/0${categoryId}`
+    fetch(url)
+    .then(res => res.json())
+    .then(status => displayArticle(status.data) );
     
 }
 
-///>>>>>>>>>>>>>News cards------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-// const loadCategories = () =>{
-//     const url = `https://openapi.programming-hero.com/api/news/categories`
-//     fetch(url)
-//     .then(res => res.json())
-//     .then(status => displayCategoies(status.data.news_category));
-// }
+// ////----------displayArticle(status.data.news_category)-------------
 
-// ////-----------------------
-const onclickCategory =() =>{
-    // console.log('abc');বাকি আছে 
-
+const displayArticle = article => {
+    const articleCard = document.getElementById('articleCard')
+    article.forEach(data => {
+        console.log(data);
+    });
 }
-
 
 loadCategories()
